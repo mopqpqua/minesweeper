622645cc4f839174c7433f8112e148ce62bbdc40
mpq <m3atyy@gmail.com>
2022-10-07 18:42:46 +0500

    feat: add aliases

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'public'),
 	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@atoms': path.resolve(__dirname, './src/components/atoms'),
+			'@mocelules': path.resolve(__dirname, './src/components/molecules'),
+			'@organisms': path.resolve(__dirname, './src/components/organisms'),
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@templates': path.resolve(__dirname, './src/templates'),
+		},
+		extensions: ['.js', '.ts'],
+	},
 	module: {
 		rules: [
 			{
